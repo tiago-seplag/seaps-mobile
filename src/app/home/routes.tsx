279e366fea76db6e801239dc4fff02580d2b706a
Tiago Bankow <tiago.bankow@gmail.com>
2025-03-06 08:01:11 -0300

    feat: checklist cards

--- src/app/home/routes.tsx
+++ src/app/home/routes.tsx
@@ -1,12 +1,23 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { HomeScreen } from ".";
+import Materialnicons from "@expo/vector-icons/MaterialIcons";
 
 const Tab = createBottomTabNavigator();
 
 export function HomeRoutes() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerTitle: "Checklists",
+          tabBarShowLabel: false,
+          tabBarIcon: () => (
+            <Materialnicons name="home" size={32} color={"#1A1A1A"} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
