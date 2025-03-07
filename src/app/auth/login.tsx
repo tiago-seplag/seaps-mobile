d6584fbdbfd251db804de7e4282f99f801dbec9f
Tiago Bankow <tiago.bankow@gmail.com>
2025-03-07 16:53:13 -0400

    chore: mt-login

--- src/app/auth/login.tsx
+++ src/app/auth/login.tsx
@@ -1,51 +1,94 @@
-import React, { createRef, useEffect } from "react";
-import { NativeSyntheticEvent, View } from "react-native";
-import { WebView, WebViewNavigation } from "react-native-webview";
-import { WebViewMessage } from "react-native-webview/lib/WebViewTypes";
+import React, { useEffect } from "react";
+import { Button } from "react-native";
 import { useSession } from "../../contexts/authContext";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-// Send the cookie information back to the mobile app
-const CHECK_COOKIE: string = `
-  ReactNativeWebView.postMessage(document.cookie);
-  true;
-`;
+import * as WebBrowser from "expo-web-browser";
+import * as AuthSession from "expo-auth-session";
+import { config } from "../../utils/mt-login";
 
-const onNavigationStateChange = (navigationState: WebViewNavigation) => {
-  if (webViewRef.current) {
-    webViewRef.current.injectJavaScript(CHECK_COOKIE);
-  }
-};
+WebBrowser.maybeCompleteAuthSession();
 
-let webViewRef = createRef<WebView>();
+const AUTH_URL =
+  "https://dev.login.mt.gov.br/auth/realms/mt-realm/protocol/openid-connect/auth";
+const TOKEN_URL =
+  "https://dev.login.mt.gov.br/auth/realms/mt-realm/protocol/openid-connect/token";
+
+const CLIENT_ID = "projeto-template-integracao"; // Seu client_id
+const REDIRECT_URI = AuthSession.makeRedirectUri({
+  scheme: "http",
+});
 
 export function Login() {
   const { signIn } = useSession();
 
-  const onMessage = async (event: NativeSyntheticEvent<WebViewMessage>) => {
-    const { data } = event.nativeEvent;
+  console.log(REDIRECT_URI);
 
-    if (data) {
-      if (data.startsWith("MT_ID_SESSION")) {
-        signIn(data);
+  const [request, response, promptAsync] = AuthSession.useAuthRequest(
+    {
+      clientId: CLIENT_ID,
+      redirectUri: "http://172.16.146.58:8081",
+      responseType: "code",
+      state: "f7fa50c1-cd43-48f0-a2ed-da08434825f7",
+      scopes: ["openid", "offline_access"],
+    },
+    { authorizationEndpoint: AUTH_URL }
+  );
+
+  async function exchangeCodeForToken(code: string) {
+    console.log("code,", code);
+
+    const body = new URLSearchParams({
+      grant_type: "authorization_code",
+      client_id: CLIENT_ID,
+      redirect_uri: REDIRECT_URI,
+      code,
+    });
+
+    try {
+      const res = await fetch(TOKEN_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: body.toString(),
+      });
+
+      const data = await res.json();
+
+      if (data.access_token) {
+        signIn(data.access_token);
+      } else {
+        console.error("Erro ao obter o token:", data);
       }
+    } catch (error) {
+      console.error("Erro na requisição:", error);
     }
-  };
+  }
+
+  useEffect(() => {
+    console.log("response", response);
+    console.log("request", request);
+
+    if (response?.type === "success") {
+      const { code } = response.params;
+      exchangeCodeForToken(code);
+    }
+  }, [response, request]);
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <WebView
+    <SafeAreaView
+      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+    >
+      <Button title="Login" disabled={!request} onPress={() => promptAsync()} />
+      {/* <WebView
         source={{
           uri: "http://172.16.146.58:3000/login",
         }}
-        ref={webViewRef}
-        onNavigationStateChange={onNavigationStateChange}
         onMessage={onMessage}
         pullToRefreshEnabled
         sharedCookiesEnabled={false}
         cacheEnabled={false}
         bounces={false}
-      />
+      /> */}
     </SafeAreaView>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { HomeRoutes } from "./app/home/routes";
 import { ChecklistRoutes } from "./app/checklist/routes";
+import { useSession } from "./contexts/authContext";
+import { Login } from "./app/auth/login";
 
 export type RootStackParamList = {
   Initial: any;
@@ -10,7 +12,9 @@ export type RootStackParamList = {
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function Index() {
-  return (
+  const { session, isLoading, signOut } = useSession();
+
+  return session ? (
     <Stack.Navigator
       screenOptions={{
         headerShown: false,
@@ -19,6 +23,15 @@ export default function Index() {
     >
       <Stack.Screen name="Initial" component={HomeRoutes} />
       <Stack.Screen name="Checklists" component={ChecklistRoutes} />
+    </Stack.Navigator>
+  ) : (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+      initialRouteName="Initial"
+    >
+      <Stack.Screen name="Initial" component={Login} />
     </Stack.Navigator>
   );
 }
