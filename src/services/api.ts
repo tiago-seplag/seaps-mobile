37875f2b18de906e71cb02712874f5045a3a675e
Tiago Bankow <tiago.bankow@gmail.com>
2025-05-22 21:57:43 -0400

    feat: 403 & 401 status code message

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError } from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Toast } from "toastify-react-native";
 
 const api = axios.create({
   baseURL: process.env.EXPO_PUBLIC_API_URL,
@@ -8,7 +9,11 @@ const api = axios.create({
 
 api.interceptors.response.use(undefined, async (data: AxiosError<any>) => {
   if (data.response) {
+    if (data.response.status === 401) {
+      Toast.error("Conta não ativa");
+    }
     if (data.response.status === 403) {
+      Toast.error("Você não tem permissão para isso");
     }
   }
   return Promise.reject(data);
